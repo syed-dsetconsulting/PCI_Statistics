fba19c19c7768c247c6d8fc8ce1d7c3da3929e1c
Syed Umer Ahmed <syed.umerahmed@dsetconsulting.com>
2025-09-27 15:58:24 +0530

    Fix to code

--- main.py
+++ main.py
@@ -708,4 +708,6 @@ def update_dashboard(days, n):
 
 
 if __name__ == "__main__":
-    app.run(debug=False, threaded=True, use_reloader=False, host='0.0.0.0', port=8050)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=False, threaded=True, use_reloader=False, host='0.0.0.0', port=port)
